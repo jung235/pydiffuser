0619e9c63c46a3a5aa293e07900e62bfc4a7f59e
jung235 <jung65537@gmail.com>
2024-06-13 12:45:38 +0900

    test: add a test file for model instantiation

---
+++ tests/models/test_models.py
@@ -0,0 +1,41 @@
+from inspect import signature
+
+import pytest
+
+from pydiffuser.models.core.base import MODEL_KWARGS
+
+
+@pytest.mark.parametrize(
+    "name",
+    [
+        ("abp"),
+        ("aoup"),
+        ("bm"),
+        ("levy"),
+        ("mips"),
+        ("rtp"),
+        ("smoluchowski"),
+        ("vicsek"),
+    ],
+)
+def test_models(config_dict, model_dict, name):
+    config = config_dict[name]()
+    model = model_dict[name].from_config(config=config)  # instantiation from config
+    ens = model.generate()
+    assert ens.microstate.shape == (config.realization, config.length, config.dimension)
+
+    ens = model.generate(realization=5, length=50, dimension=2)
+    with pytest.raises(AssertionError):
+        assert ens.microstate.shape == (5, 50, 2)
+
+    kwargs = config_dict[name].show_fields()
+    params = list(signature(model_dict[name].__init__).parameters.keys())
+    args = (
+        [kwargs[param] for param in params[1:]]
+        if MODEL_KWARGS not in params
+        else ([kwargs[param] for param in params[1:-1]])
+    )
+    model_v2 = model_dict[name](*args)  # instantiation without config
+
+    ens_v2 = model_v2.generate(realization=5, length=50, dimension=2)
+    assert ens_v2.microstate.shape == (5, 50, 2)
