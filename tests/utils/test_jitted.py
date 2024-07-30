af562f719482193e84912df097d71d8d16458c3c
jung235 <jung65537@gmail.com>
2024-07-30 20:48:42 +0900

    feat: add the test code for `jitted`

--- tests/utils/test_jitted.py
+++ tests/utils/test_jitted.py
@@ -8,7 +8,12 @@ from numpy.random import rand, randint, uniform
 from scipy.stats import expon, norm, pareto
 
 import pydiffuser as pyd
-from pydiffuser.utils.jitted import get_noise, normalize
+from pydiffuser.utils.jitted import (
+    get_noise,
+    normalize,
+    polar_to_cartesian,
+    spherical_to_cartesian,
+)
 
 
 def test_normalize():
@@ -21,6 +26,32 @@ def test_normalize():
     normed_arr = normalize(arr=arr, axis=0)
     expected_arr = jnp.array([[0.6], [0.8]])
     assert jnp.all(normed_arr == expected_arr)
+
+
+@pytest.mark.parametrize(
+    "r, phi",
+    [
+        (1, [jnp.pi / 6, jnp.pi / 4, jnp.pi / 3, jnp.pi / 2, jnp.pi]),
+        (2, [jnp.pi / 6, jnp.pi / 4, jnp.pi / 3, jnp.pi / 2, jnp.pi]),
+    ],
+)
+def test_polar_to_cartesian(r, phi):
+    phi = jnp.array(phi)
+    x1, x2 = polar_to_cartesian(r, phi)
+    assert jnp.all(x1 == r * jnp.cos(phi))
+    assert jnp.all(x2 == r * jnp.sin(phi))
+
+
+def test_spherical_to_cartesian():
+    r = 1
+    theta = rand(50) * jnp.pi
+    phi = rand(50) * 2 * jnp.pi
+    x1, x2, x3 = spherical_to_cartesian(
+        r=r, theta=theta.reshape(5, 10), phi=phi.reshape(5, 10)
+    )
+    assert x1.shape == (5, 10)
+    assert x2.shape == (5, 10)
+    assert x3.shape == (5, 10)
 
 
 @pytest.mark.parametrize(
